64d57d571103d4745205c0f3aa303272784b67be
Patryk <dolata.patryk1@gmail.com>
2018-10-27 15:51:53 +0200

    added libs, added connection to database by hikari

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,18 @@
 package application;
 	
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import utils.Db;
 
 
 public class Main extends Application {
+	Logger logger = LoggerFactory.getLogger(Main.class);
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -15,8 +21,10 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			Db.addConnection();
 		} catch(Exception e) {
-			e.printStackTrace();
+			logger.warn(e.getMessage());
 		}
 	}
 	
--- src/utils/Db.java
+++ src/utils/Db.java
@@ -1,7 +1,44 @@
 package utils;
 
-import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-public @Data class Db {
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
+public class Db {
+	
+	static Logger logger = LoggerFactory.getLogger(Db.class);
+
+	private static ConnectionData dbConnection;
+	
+	public static void addConnection() {
+		try {
+			HikariConfig config = new HikariConfig();
+			config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+			config.setJdbcUrl("jdbc:mysql://localhost:3306/mma?"
+					+ "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+			config.setUsername("patryk");
+			config.setPassword("dromader15");
+			config.setMinimumIdle(1);
+			config.setMaximumPoolSize(3);
+
+			HikariDataSource hikariDataSource = new HikariDataSource(config);
+			dbConnection = new ConnectionData(hikariDataSource, new JdbcTemplate(hikariDataSource), 
+					new NamedParameterJdbcTemplate(hikariDataSource));
+			System.out.println("Connected to db");
+		} catch (Exception e) {
+			logger.warn(e.getMessage());
+		}
+	}
+	
+	public static JdbcTemplate getJdbcTemplate() {
+		return dbConnection.getJdbcTemplate();
+	}
+	
+	public static NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+		return dbConnection.getNamedJdbcTemplate();
+	}
 }
