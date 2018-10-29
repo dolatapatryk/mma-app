54319f6dab01120c4759472c03e9799fd716fb2e
Patryk <dolata.patryk1@gmail.com>
2018-10-29 21:23:32 +0100

    fixes

--- src/utils/ConnectionData.java
+++ src/utils/ConnectionData.java
@@ -5,11 +5,8 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import com.zaxxer.hikari.HikariDataSource;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 
 @Data @AllArgsConstructor public class ConnectionData {
 
